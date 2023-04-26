82ebb963ee1c72ea5bb738d04afbb652d8da2eac
ManuelB23 <98127595+ManuelB23@users.noreply.github.com>
2023-04-25 19:41:37 -0500

    Ciclo 3 finalizado
    El metodo refresh funciona, los cuadros de la pantalla despues del primer click mantienen su color.

--- src/presentation/Conecta4GUI.java
+++ src/presentation/Conecta4GUI.java
@@ -1,6 +1,7 @@
 package presentation;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
@@ -8,14 +9,28 @@ import java.io.File;
 
 public class Conecta4GUI extends JFrame {
 
+    private static final int NUM_ROWS = 5;
+    private static final int NUM_COLS = 5;
+
+    private int[][] tablero = new int[NUM_ROWS][NUM_COLS];
+
+    private JButton[][] buttons = new JButton[NUM_ROWS][NUM_COLS];
     public JMenuBar barra;
     public JMenu menu;
-    public JMenuItem New, Open, Saved, Exit;
+    public JMenuItem New, Open, Saved, Refresh, Exit;
     public JFileChooser Seleccion;
+    public JLabel jugador1Label, jugador2Label, tiempoLabel;
+    public JPanel infoPanel;
 
+    private enum Player {
+        PLAYER_1, PLAYER_2
+    }
+    private Player currentPlayer = Player.PLAYER_1;
     public Conecta4GUI(){
         prepareElements();
         prepareAccions();
+        prepareElementsBoard(5,5);
+        refresh();
     }
 
     private void prepareElements() {
@@ -24,7 +39,36 @@ public class Conecta4GUI extends JFrame {
         setBounds(0,0,Screen.width/2,Screen.height/2);
         setLocationRelativeTo(null);
         prepareElementsMenu();
+
+        infoPanel = new JPanel();
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.PAGE_AXIS));
+        infoPanel.setPreferredSize(new Dimension(300, 10));
+
+
+        infoPanel.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK),
+                new EmptyBorder(10, 100, 10, 10)));
+
+        jugador1Label = new JLabel("Player 1: Red", SwingConstants.CENTER);
+        jugador1Label.setOpaque(true);
+        jugador1Label.setBackground(Color.RED);
+        jugador1Label.setForeground(Color.WHITE);
+
+        jugador2Label = new JLabel("Player 2: Blue", SwingConstants.CENTER);
+        jugador2Label.setOpaque(true);
+        jugador2Label.setBackground(Color.BLUE);
+
+        tiempoLabel = new JLabel("Turno: 0", SwingConstants.CENTER);
+
+        infoPanel.add(Box.createVerticalGlue());
+        infoPanel.add(jugador1Label);
+        infoPanel.add(jugador2Label);
+        infoPanel.add(tiempoLabel);
+        infoPanel.add(Box.createVerticalGlue());
+
+        getContentPane().add(infoPanel, BorderLayout.WEST);
     }
+
 
     private void prepareElementsMenu() {
         barra = new JMenuBar();
@@ -33,12 +77,15 @@ public class Conecta4GUI extends JFrame {
         New = new JMenuItem("New");
         Open = new JMenuItem("Open");
         Saved = new JMenuItem("Saved");
+        Refresh = new JMenuItem("Refresh");
         Exit = new JMenuItem("Exit");
         menu.add(New);
         menu.addSeparator();
         menu.add(Open);
         menu.addSeparator();
         menu.add(Saved);
+        menu.addSeparator();
+        menu.add(Refresh);
         menu.addSeparator();
         menu.add(Exit);
         setJMenuBar(barra);
@@ -75,12 +122,61 @@ public class Conecta4GUI extends JFrame {
                 saved();
             }
         });
+        Refresh.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                refresh();
+            }
+        });
         Exit.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 exit();
             }
         });
+    }
+
+    private void prepareElementsBoard(int numRows, int numCols) {
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+
+        int buttonWidth = 150 / numCols;
+        int buttonHeight = 350 / numRows;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                buttons[row][col] = new JButton();
+                gbc.gridx = col;
+                gbc.gridy = row;
+                gbc.weightx = 3.0 / numCols;
+                gbc.weighty = 3.0 / numRows;
+                gbc.ipadx = buttonWidth;
+                gbc.ipady = buttonHeight;
+                panel.add(buttons[row][col], gbc);
+                buttons[row][col].addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        JButton clickedButton = (JButton) e.getSource();
+                        if (clickedButton.getBackground().equals(UIManager.getColor("Button.background"))) {
+                            if (currentPlayer == Player.PLAYER_1) {
+                                clickedButton.setBackground(Color.RED);
+                                currentPlayer = Player.PLAYER_2;
+                            } else {
+                                clickedButton.setBackground(Color.BLUE);
+                                currentPlayer = Player.PLAYER_1;
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        int margin = 100;
+        panel.setBorder(BorderFactory.createEmptyBorder(margin, margin, margin, margin));
+
+        getContentPane().add(panel, BorderLayout.EAST);
     }
 
     private void saved() {
@@ -104,6 +200,8 @@ public class Conecta4GUI extends JFrame {
     private void New() {
         JOptionPane.showMessageDialog(null, "Este Item todavía no está implementado");
     }
+
+
     private void exit() {
         int result = JOptionPane.showConfirmDialog(rootPane, "Are you sure you want to exit?", "Exit", JOptionPane.YES_NO_OPTION);
         if (result == JOptionPane.YES_OPTION) {
@@ -112,4 +210,17 @@ public class Conecta4GUI extends JFrame {
         }
     }
 
+    private void refresh() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                tablero[row][col] = 0;
+                buttons[row][col].setBackground(UIManager.getColor("Button.background"));
+                buttons[row][col].setEnabled(true);
+            }
+        }
+        currentPlayer = Player.PLAYER_1;
+        jugador1Label.setBackground(Color.RED);
+        jugador2Label.setBackground(Color.BLUE);
+        tiempoLabel.setText("Turno: 0");
+    }
 }
